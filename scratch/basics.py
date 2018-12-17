37e3e90f96483795ec0268362e62a24eccced599
RichardDW <richard.dewolff@gmail.com>
2018-12-17 16:05:02 +0100

    Updates on general stuff

--- scratch/basics.py
+++ scratch/basics.py
@@ -36,3 +36,71 @@ primesS = set((1,2,3,5,7))
 primesT = (1,2,3,5,7)
 #create a list
 primesL = [1,2,3,5,7]
+
+## Test if item is in list
+r = range(1,5)
+# test if value in range
+1 in r
+
+# Web Scraping
+main_url = "https://www.humblebundle.com/"
+
+import requests
+result = requests.get(main_url)
+
+result.text[:1000]
+
+
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(result.text, 'html.parser')
+
+print(soup.prettify()[:1000])
+
+# Collections ##############
+import collections as col
+
+abc = 'jiwamotqgcfnudclzbyxkzmrvp'
+mycount = col.Counter(abc)
+print(2 in mycount.values())
+
+# show type 
+mycount.elements()
+# show elements of collection
+list(mycount.elements())
+# show number of elements
+len(list(mycount.elements()))
+
+
+############ Python OOP #######################
+############ Create a class  ###########
+
+class Employee:
+
+	def __init__(self, first, last, pay):
+	    self.first = first
+	    self.last = last
+	    self.pay = pay
+	    self.email = first + '.' + last + '@company.com'
+
+
+emp_1 = Employee('Corey', 'Schafer', 50000)
+emp_2 = Employee('Test', 'User', 40000)
+
+# to print full name we can do:
+print('{} {}'.format(emp_1.first,emp_1.last))
+
+# or we create a method:
+
+class Employee:
+
+	def __init__(self, first, last, pay):
+		self.first = first
+		self.last = last
+		self.pay = pay
+		self.email = first + '.' + last + '@company.com'
+		
+	def fullname(self):
+		return '{} {}'.format(self.first,self.last)
+
+
+print(emp_1.fullname)
