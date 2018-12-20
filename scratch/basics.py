3a6766422a0be4be8bd0edbf831afee336a0eb96
RichardDW <richard.dewolff@gmail.com>
2018-12-20 12:48:23 +0100

    extended Class

--- scratch/basics.py
+++ scratch/basics.py
@@ -95,6 +95,8 @@ print('{} {}'.format(emp_1.first,emp_1.last))
 
 class Employee:
 
+    raise_amount = 1.04
+
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
@@ -105,7 +107,7 @@ class Employee:
         return '{} {}'.format(self.first, self.last)
 
     def apply_raise(self):
-        self.pay = int(self.pay * 1.04)
+        self.pay = int(self.pay * self.raise_amount)
 
 
 emp_1 = Employee('Corey', 'Schafer', 50000)
@@ -117,5 +119,11 @@ print(Employee.fullname(emp_1))
 print(emp_1.pay)
 emp_1.apply_raise()
 print(emp_1.pay)
+print(Employee.raise_amount)
+print(emp_1.raise_amount)
+print(emp_1.__dict__)
+print(Employee.__dict__)
+
+
 
 
