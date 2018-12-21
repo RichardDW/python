c147dc100c97f49570061e30fdc845a15cdaa424
RichardDW <richard.dewolff@gmail.com>
2018-12-21 14:45:54 +0100

    Updates to day2 of AoC

--- AoC2018/advent02.py
+++ AoC2018/advent02.py
@@ -3,22 +3,21 @@ import difflib as diff
 
 
 def compare_letters(inpList):
-    enen = 0
-    tween = 0
-    drien = 0
+    has1 = 0
+    has2 = 0
+    has3 = 0
     for letters in inpList:
         my_counter=Counter(letters).values()
         # print(my_counter)
         # check if there is a 2 or 3 value
-        if 1 in my_counter:
-            enen += 1
-        if 2 in my_counter:
-            tween += 1
-            # print(f'there is a 2 in the list')
-        if 3 in my_counter:
-            drien += 1
-            # print(f'there is a 3 in the list')
-    print(f'enen zijn met {enen}, tween zijn met {tween} en drien zijn met zn {drien}')
+        # since there are single characters in each line, the
+        # total of enen must be the same as number of lines in inpList.
+        has1 += 1 in my_counter
+        has2 += 2 in my_counter
+        has3 += 3 in my_counter
+
+    print(f'ones are with {has1}, twos are with {has2} and threes are with {has3}')
+    return has2 * has3
 
 
 def lookup_letters(inpList):
@@ -43,14 +42,14 @@ def lookup_letters(inpList):
 
 def main():
     # Initialize variables
-    input_file="d:\\DL\\input02.txt"
+    input_file = "d:\\DL\\input02.txt"
     # read input_file, strip newline and store in inputList
     input_list = [inputTxt.strip() for inputTxt in open(input_file, "r").readlines()]
     # print number of items in list
       
-    compare_letters(input_list)
+    print(compare_letters(input_list))
 
-    lookup_letters(input_list)
+    #lookup_letters(input_list)
 
 
 if __name__ == "__main__":
