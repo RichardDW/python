d97e83b78cf3361aa732fd9df478b0d2785e4f74
RichardDW <richard.dewolff@gmail.com>
2019-03-27 09:05:36 +0100

    Little Math project

---
+++ basics/max.py
@@ -0,0 +1,17 @@
+# multiply all digits of a number and repeat.
+# search for a string that has the most steps to 
+# repeat this process
+
+def per(n):
+	if len(str(n))==1:
+		print(n)
+		return "DONE"
+		
+	digits = [int(i) for i in str(n)]
+	
+	result = 1
+	for j in digits:
+		result *= j
+	print(result)
+
+	per(result)
