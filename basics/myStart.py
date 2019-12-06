2877803301f7b6d27673b0271ea0aab4fdf87949
RichardDW <richard.dewolff@gmail.com>
2019-12-06 19:03:58 +0100

    updates..

--- basics/myStart.py
+++ basics/myStart.py
@@ -2,5 +2,26 @@ def main():
     print("hello world")
 
 
+# Fizz Buzz
+for num in range(1,101):
+    if num % 5 == 0 and num % 3 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
+
+
+# Fibonacci sequence
+a, b = 0, 1
+for i in range(0,20):
+    print(a)
+    a, b = b, a + b
+
+
+
+
 if __name__ == "__main__":
     main()
